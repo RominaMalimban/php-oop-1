cdc2f13ff76942a34c0093bd0ae9644ad02e7bf6
RominaMalimban <romina.lyn@hotmail.it>
2023-01-24 16:32:19 +0100

    modifica nomi variabili e rimozione <br> in funzione

--- index.php
+++ index.php
@@ -20,18 +20,17 @@ class Movie
     public function getHtml()
     {
         return "<h1>Title: " . $this->title . "</h1>"
-            . "<br><h3>Running Time: " . $this->runningTime . "<h3>"
-            . "<br><h3>Language " . $this->language . "</h3>"
-            . "<br>";
+            . "<h3>Running Time: " . $this->runningTime . "<h3>"
+            . "<h3>Language " . $this->language . "</h3>";
     }
 }
 
 //Istanzio due oggetti `Movie` e stampo a schermo i valori delle relative proprietà:
-$movie1 = new Movie("Babylon", "188 min", "english");
-$movie2 = new Movie("Kill Bill Volume 1", "111 min", "english");
+$babylon = new Movie("Babylon", "188 min", "english");
+$killBill = new Movie("Kill Bill Volume 1", "111 min", "english");
 
-// creo array Movies e ciclo:
-$movies = [new Movie("Babylon", "188 min", "english"), new Movie("Kill Bill Volume 1", "111 min", "english")];
+// creo array Movies e ciclo con foreach:
+$movies = [$babylon, $killBill];
 
 foreach ($movies as $movie) {
     echo $movie->getHtml();
