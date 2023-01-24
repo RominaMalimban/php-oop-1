23275c72a6fe6f7d5ec153ffcf4a591706d3a5b2
RominaMalimban <romina.lyn@hotmail.it>
2023-01-24 15:36:02 +0100

    definizione costruttore

--- index.php
+++ index.php
@@ -7,4 +7,12 @@ class Movie
     public $title;
     public $runningTime;
     public $language;
+
+    //  definisco un costruttore:
+    public function __construct($title, $runningTime, $language)
+    {
+        $this->title = $title;
+        $this->runningTime = $runningTime;
+        $this->language = $language;
+    }
 }
