b6040487b510ed4662e3105c3de2edb797988b13
RominaMalimban <romina.lyn@hotmail.it>
2023-01-24 16:20:14 +0100

    creazionw array e ciclo foreach

--- index.php
+++ index.php
@@ -19,17 +19,20 @@ class Movie
     //  definisco un metodo:
     public function getHtml()
     {
-        return "Title: " . $this->title
-            . "<br>Running Time: " . $this->runningTime
-            . "<br>Language " . $this->language;
+        return "<h1>Title: " . $this->title . "</h1>"
+            . "<br><h3>Running Time: " . $this->runningTime . "<h3>"
+            . "<br><h3>Language " . $this->language . "</h3>"
+            . "<br>";
     }
 }
 
 //Istanzio due oggetti `Movie` e stampo a schermo i valori delle relative proprietà:
-
-// Oggetto 1:
 $movie1 = new Movie("Babylon", "188 min", "english");
 $movie2 = new Movie("Kill Bill Volume 1", "111 min", "english");
 
-echo $movie1->getHtml();
-echo $movie2->getHtml();
+// creo array Movies e ciclo:
+$movies = [new Movie("Babylon", "188 min", "english"), new Movie("Kill Bill Volume 1", "111 min", "english")];
+
+foreach ($movies as $movie) {
+    echo $movie->getHtml();
+}
